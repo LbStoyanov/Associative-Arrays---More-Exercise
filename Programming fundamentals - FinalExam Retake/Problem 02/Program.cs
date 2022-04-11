25a3bf8960b4fc700420e8721b24de67f4789a9c
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-04-11 13:01:04 +0300

    Second problem 50% sovled.Added last problem

--- Programming fundamentals - FinalExam Retake/Problem 02/Program.cs
+++ Programming fundamentals - FinalExam Retake/Problem 02/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
 
 namespace Problem_02_
 {
@@ -6,7 +8,51 @@ namespace Problem_02_
     {
         static void Main(string[] args)
         {
-            
+            string message = Console.ReadLine();
+
+            while (message != "Last note")
+            {
+                Regex pattern = new Regex(@"\b(?<peakName>[^\.\,\:][A-Za-z!@#$?]+?)=(?<geohashlenght>[0-9]+?)<<(?<geohashcode>.+)\b");
+
+                Match match = pattern.Match(message);
+
+                if (match.Success)
+                {
+                    string nameOfMountain = match.Groups["peakName"].Value;
+                    string hashCode = match.Groups["geohashcode"].Value;
+                    int hashCodeLenght = int.Parse(match.Groups["geohashlenght"].Value);
+
+                    if (hashCodeLenght != hashCode.Length)
+                    {
+                        Console.WriteLine("Nothing found!");
+                        message = Console.ReadLine();
+                        continue;
+                    }
+                    nameOfMountain = ExtractWord(nameOfMountain);
+                    Console.WriteLine($"Coordinates found! {nameOfMountain} -> {hashCode}");
+                }
+                else
+                {
+                    Console.WriteLine("Nothing found!");
+                }
+
+                message = Console.ReadLine();
+            }
+        }
+
+        static string ExtractWord(string nameOfMountain)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < nameOfMountain.Length; i++)
+            {
+                var currenChar = nameOfMountain[i];
+                if (char.IsLetter(currenChar)||char.IsDigit(currenChar))
+                {
+                    sb.Append(currenChar);
+                }
+            }
+            return sb.ToString();
         }
     }
 }
