e8fd8c6d54f8ee1e07dff2f28a809197f88d9666
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-06-07 23:21:46 +0300

    Done!

---
+++ EarlyExamSoftUni/01. PC Store.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace EarlyExamSoftUni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double Dollarscurs = 1.57;
+
+            double proccesorPriceInDollars = double.Parse(Console.ReadLine());
+            double videoCardPriceInDollars = double.Parse(Console.ReadLine());
+            double ramPriceInDollars = double.Parse(Console.ReadLine());
+
+            double ramQuantity = double.Parse(Console.ReadLine());
+            double discount = double.Parse(Console.ReadLine());
+
+            double proccesorPriceInlv = proccesorPriceInDollars * Dollarscurs;
+            double videoCardPriceInLeva = videoCardPriceInDollars * Dollarscurs;
+            double ramPriceInLeva = (ramPriceInDollars * Dollarscurs) * ramQuantity;
+            double totalPriceForProccesorAndVideoCardPrice = proccesorPriceInlv + videoCardPriceInLeva;
+            double finalPriceForProccesorAndVideoCardAfterDiscount = totalPriceForProccesorAndVideoCardPrice - (totalPriceForProccesorAndVideoCardPrice * discount);
+            double totalAmount = finalPriceForProccesorAndVideoCardAfterDiscount + ramPriceInLeva;
+            Console.WriteLine($"Money needed - {totalAmount:f2} leva.");
+
+        }
+    }
+}
